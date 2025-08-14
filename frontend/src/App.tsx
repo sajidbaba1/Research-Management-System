da59036b73eda237ac0cdd1b8b4e151f9cb75ce9
sajid <thsajid831@gmail.com>
2025-08-14 21:30:17 +0530

    working crud completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Add react-router-dom: npm install react-router-dom @types/react-router-dom
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ProjectList from './components/ProjectList';
+import ProjectTimeline3D from './components/ProjectTimeline3D';
 
 const App: React.FC = () => {
-  return (
-      <Router>
-        <div className="min-h-screen bg-gray-100">
-          <Routes>
-            <Route path="/" element={<ProjectList />} />
-            {/* Add more routes later */}
-          </Routes>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div className="min-h-screen bg-gray-100">
+                <nav className="bg-blue-500 p-4">
+                    <ul className="flex space-x-4 text-white">
+                        <li><Link to="/">Project List</Link></li>
+                        <li><Link to="/timeline">3D Timeline</Link></li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<ProjectList />} />
+                    <Route path="/timeline" element={<ProjectTimeline3D />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
